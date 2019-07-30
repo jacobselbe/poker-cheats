89cf4224b115c954074090c722afd15db204a455
Jacob Selbe <jalxselbe@gmail.com>
2019-07-30 02:33:56 -0500

    Updates

---
+++ precache-manifest.50d6b3ac77b4e4317be51da594c010aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10523963c6affd294eba6f7d3322f72c",
+    "url": "/poker-cheats/index.html"
+  },
+  {
+    "revision": "82c1e8a9556602a14775",
+    "url": "/poker-cheats/static/css/main.87becf0c.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/poker-cheats/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "82c1e8a9556602a14775",
+    "url": "/poker-cheats/static/js/main.865373d8.chunk.js"
+  },
+  {
+    "revision": "76a1dfe1f907aa51817f",
+    "url": "/poker-cheats/static/js/runtime~main.26c9047d.js"
+  },
+  {
+    "revision": "aa5d94edc3555714398c747837e60050",
+    "url": "/poker-cheats/static/media/logo.aa5d94ed.svg"
+  }
+]);
