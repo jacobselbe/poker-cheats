2bbadec9f88fe07f75f728183af39cfe53d41406
Jacob Selbe <jalxselbe@gmail.com>
2019-07-31 04:29:46 -0500

    setting up state management

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+
+// import other components needed
+// import actions needed
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Under construction. Come back soon.</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+// change too class based component
+
+// export class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+
+//       </div>
+//     );
+//   }
+// }
+
+// App.defaultProps = {
+  
+// };
+
+// const mapStateToProps = state => ({
+  
+// });
+
+// export default connect(mapStateToProps)(App);
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+
+import { something } from './reducers';
+
+export default createStore(something);
